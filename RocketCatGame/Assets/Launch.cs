5dbeb8f9ea54d662805feb35a6071e92222f37e6
AuraQuinn <38054991+AuraQuinn@users.noreply.github.com>
2018-05-01 21:38:57 -0400

    UI Added
    Launch Power UI added.

--- RocketCatGame/Assets/Launch.cs
+++ RocketCatGame/Assets/Launch.cs
@@ -3,13 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Launch : MonoBehaviour {
+    public float LaunchRotation;
+    public float LaunchPower;
+    public bool canRotate = true;
+    public bool canLaunch = false;
+
 
 	// Use this for initialization
 	void Start () {
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        print(Mathf.Sin(Time.time * 5) * 3);
+
+    // Update is called once per frame
+    void Update () {
+        if (canRotate == true)
+        {
+            LaunchRotation = (Mathf.Sin(Time.time * 2) * 1);
+            transform.Rotate(Vector3.forward * (LaunchRotation * 2));
+        }
+        if (canLaunch == true)
+        {
+            LaunchPower = (Mathf.Sin(Time.time * 2) * 1);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (canRotate == true)
+            {
+                canRotate = false;
+                canLaunch = true;
+                print(LaunchRotation);
+                print(canRotate);
+                print(canLaunch);
+            }
+            else if (canLaunch == true)
+            {
+                canLaunch = false;
+                print(LaunchPower);
+                print(canLaunch);
+            }
+        }
 	}
 }
---
+++ RocketCatGame/Assets/PowerUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUI : MonoBehaviour {
+    public GameObject Launcher;
+    private Launch LaunchRef;
+    public float LaunchPowerRef;
+
+	// Use this for initialization
+	void Start () {
+        LaunchRef = Launcher.GetComponent<Launch>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        LaunchPowerRef = LaunchRef.LaunchPower;
+        transform.localScale = new Vector3(0.25F, LaunchPowerRef + 1, 1);
+	}
+}
